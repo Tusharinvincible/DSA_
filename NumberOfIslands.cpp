ecd0c515a368d5ef14e08b6a9edc929f828f722a
Tusharinvincible <tusharvarkhede363@gmail.com>
2021-09-07 18:43:51 +0530

    No. Of Islands Added

--- NumberOfIslands.cpp
+++ NumberOfIslands.cpp
@@ -1,3 +1,9 @@
+//Given a square chessboard, the initial position of Knight and position of a target. 
+//Find out the minimum steps a Knight will take to reach the target position.
+
+//Note:
+//The initial and the target position co-ordinates of Knight have been given accoring to 1-base indexing.
+
 #include <bits/stdc++.h>
 
 using namespace std;
